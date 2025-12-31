fd3c668727ee0842c366925c9d05b614602c4a31
FranNavarroTH <128621958+FranNavarroTH@users.noreply.github.com>
2025-12-31 08:35:49 +0100

    Update Empleado.java
    Empleado ahora hereda de Usuario

--- src/main/java/com/easifood/app/model/Empleado.java
+++ src/main/java/com/easifood/app/model/Empleado.java
@@ -5,13 +5,13 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "empleados")
-public class Empleado {
+public class Empleado extends Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre;
+    //private String nombre;
     private String puesto;
     private BigDecimal salario;
 
@@ -21,8 +21,24 @@ public class Empleado {
 
     public Empleado() {}
 
-    public Empleado(String nombre, String puesto, BigDecimal salario, Restaurante restaurante) {
-        this.nombre = nombre;
+    /*public Empleado(String nombre, String puesto, BigDecimal salario, Restaurante restaurante) {
+        //this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+        this.restaurante = restaurante;
+    }*/
+
+    public Empleado(String nombre,
+                    String apellidos,
+                    String correo,
+                    String imagen,
+                    String contra,
+                    String role,
+                    String puesto,
+                    BigDecimal salario,
+                    Restaurante restaurante) {
+
+        super(nombre, apellidos, correo, imagen, contra, role);
         this.puesto = puesto;
         this.salario = salario;
         this.restaurante = restaurante;
@@ -30,8 +46,8 @@ public class Empleado {
 
     // Getters y Setters
     public Long getId() { return id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    //public String getNombre() { return nombre; }
+    //public void setNombre(String nombre) { this.nombre = nombre; }
     public String getPuesto() { return puesto; }
     public void setPuesto(String puesto) { this.puesto = puesto; }
     public BigDecimal getSalario() { return salario; }
